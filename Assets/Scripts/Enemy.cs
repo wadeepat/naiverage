00dde303b4311dd392748dacafe6821167ee7de8
vVviewVv <viewissara1246@gmail.com>
2022-07-18 12:44:56 +0700

    Inventory and hp bar

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+// public class Enemy : Interactable 
+// {
+//     // Start is called before the first frame update
+//     public override void Interact(){
+//         base.Interact();
+//         // attack the enemy
+//     }
+// }
---
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Inventory inventory;
+    void Start()
+    {
+        // Inventory.instance;
+        // Inventory.onItemChangedCallback += Update;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // void UpdateUI()
+    // {
+    //     Debug.Log("UPDATING UI");
+    // }
+}
