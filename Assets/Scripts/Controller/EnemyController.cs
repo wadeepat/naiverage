2bf28ef989f0816aa2b661f3e7bf32aa2024dbc5
wadeepat <leaw.wadeepat@gmail.com>
2022-07-21 19:11:05 +0700

    animate the spider

--- Assets/Scripts/Controller/EnemyController.cs
+++ Assets/Scripts/Controller/EnemyController.cs
@@ -5,40 +5,54 @@ using System.Collections;
 public class EnemyController : MonoBehaviour
 {
     public float lookRadius = 10f;
-
+    public float stopRadius = 2f;
     Transform target;
     NavMeshAgent agent;
-
+    private Animator _anim;
 
     // Start is called before the first frame update
     void Start()
     {
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
+        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float distance = Vector3.Distance(target.position, transform.position);
-        if (distance <= lookRadius){
+        Debug.Log("distance" + distance);
+        Debug.Log("lookRadius" + lookRadius);
+        if (distance <= lookRadius && distance >= stopRadius)
+        {
+            _anim.SetBool("isWalking", true);
+
             agent.SetDestination(target.position);
-            
-            if(distance <= lookRadius){
+
+            if (distance <= lookRadius && distance >= stopRadius)
+            {
                 //Attack
                 //Face
                 FaceTarget();
             }
+
+
+        }
+        else
+        {
+            _anim.SetBool("isWalking", false);
         }
     }
 
-    void FaceTarget (){
+    void FaceTarget()
+    {
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 
-    void OnDrawGizmosSelected ()
+    void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, lookRadius);
