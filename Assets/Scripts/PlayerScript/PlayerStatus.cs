1d17c6061e892342553a4eceb1248fcbcd9cc1aa
wadeepat <leaw.wadeepat@gmail.com>
2022-08-12 12:57:38 +0700

    edit some scene

--- Assets/Scripts/PlayerScript/PlayerStatus.cs
+++ Assets/Scripts/PlayerScript/PlayerStatus.cs
@@ -1,33 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerStatus : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private int HP = 100;
-    //[SerializeField] private GameObject healthBar;
-    // private Slider slider;
+    [SerializeField] private GameObject healthBar;
+    private Slider slider;
     private int hp;
     void Start()
     {
+        slider = healthBar.GetComponent<Slider>();
         hp = HP;
     }
 
     // Update is called once per frame
-    //void Update()
-    //{
-    // if (hp >= 0)
-    //     slider.value = (float)hp / (float)HP;
-    // else
-    //     slider.value = 0;
-    //}
+    void Update()
+    {
+        if (hp >= 0)
+            slider.value = (float)hp / (float)HP;
+        else
+            slider.value = 0;
+    }
     public void TakeDamaged(int damageAmount)
     {
         hp -= damageAmount;
         Debug.Log("Hited left" + hp);
         if (hp <= 0)
         {
+            hp = 0;
             // healthBar.SetActive(false);
             // animator.SetTrigger("die");
             // GetComponent<BoxCollider>().enabled = false;
