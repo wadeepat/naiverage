2835441b93e61d9f89815b7f16b8c0b007eddbf8
wadeepat <leaw.wadeepat@gmail.com>
2022-12-18 18:30:25 +0700

    edit skeleton animation

--- Assets/Scripts/Objects/MonsterSpawn.cs
+++ Assets/Scripts/Objects/MonsterSpawn.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class MonsterSpawn : MonoBehaviour
 {
+    [Header("Monster Details")]
     [SerializeField] private GameObject monster;
+    [SerializeField] private float minScale = -0.1f;
+    [SerializeField] private float maxScale = 0.1f;
     private GameObject WaypointObject;
     private List<Transform> waypoints = new List<Transform>();
     void Start()
@@ -14,13 +17,15 @@ public class MonsterSpawn : MonoBehaviour
         {
             waypoints.Add(wp);
         }
-        SpawnMonster(2);
+        SpawnMonster(5);
     }
 
     public void SpawnMonster(int amount)
     {
         for (int i = 0; i < amount; i++)
         {
+            var randomNum = Random.Range(minScale, maxScale);
+            monster.transform.localScale += new Vector3(randomNum, randomNum, randomNum);
             var tempMon = Instantiate(monster, waypoints[Random.Range(0, waypoints.Count)].position, Quaternion.identity);
             tempMon.transform.SetParent(this.transform);
         }
